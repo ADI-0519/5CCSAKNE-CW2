709e8ec5f0cd4aa6c182753db86f7fb2a62cd9c5
Yingtao Zheng <zhengkevintao@gmail.com>
2026-04-04 23:02:39 +0100

    Add data_collection from structured data source NEWSAPI for "today"

--- src/data_collection.py
+++ src/data_collection.py
@@ -1,8 +1,37 @@
+import requests
+from datetime import date
+import os
+from dotenv import load_dotenv
+
+#config
+load_dotenv()
+today = date.today()
+config = {
+    "NEWS_API_KEY": os.getenv("NEWS_API_KEY"),
+    "GUARDIAN_API_KEY": os.getenv("GUARDIAN_API_KEY"),
+    "Keywords": ["AI", "Machine Learning", "Robotics", "Artificial Intelligence"]
+}
+
+
 def data_collection():
     print("Collecting data...")
-    # Code to collect data goes here
-    # This could involve web scraping, API calls, or reading from files
-    # For example:
-    # data = scrape_website("https://example.com/data")
-    # save_data_to_file(data, "raw_data.json")
-    pass
+
+# Extract news articles about AI
+    url = ('https://newsapi.org/v2/everything?'
+        f'q={" OR ".join(config["Keywords"])}&'
+        f'from={today}&'
+        'sortBy=popularity&'
+        f'apiKey={os.getenv("NEWS_API_KEY")}')
+    response = requests.get(url)
+
+    print(response.json())
+
+# # Extract top-headlines
+#     url2 = ('https://newsapi.org/v2/top-headlines?'
+#         'country=us&'
+#         f'apiKey={os.getenv("NEWS_API_KEY")}')
+#     response2 = requests.get(url2)
+#     print(response2.json())
+
+
+data_collection()
