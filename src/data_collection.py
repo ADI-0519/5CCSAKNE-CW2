e40268505c7f89585cc63e314e2099e853bdfc4d
Yingtao Zheng <zhengkevintao@gmail.com>
2026-04-04 23:04:53 +0100

    Add config, move code from data_collection to main

--- src/data_collection.py
+++ src/data_collection.py
@@ -1,37 +1,10 @@
 import requests
-from datetime import date
-import os
-from dotenv import load_dotenv
 
-#config
-load_dotenv()
-today = date.today()
-config = {
-    "NEWS_API_KEY": os.getenv("NEWS_API_KEY"),
-    "GUARDIAN_API_KEY": os.getenv("GUARDIAN_API_KEY"),
-    "Keywords": ["AI", "Machine Learning", "Robotics", "Artificial Intelligence"]
-}
-
-
-def data_collection():
+def load_data_from_url(url):
     print("Collecting data...")
-
-# Extract news articles about AI
-    url = ('https://newsapi.org/v2/everything?'
-        f'q={" OR ".join(config["Keywords"])}&'
-        f'from={today}&'
-        'sortBy=popularity&'
-        f'apiKey={os.getenv("NEWS_API_KEY")}')
     response = requests.get(url)
-
-    print(response.json())
-
-# # Extract top-headlines
-#     url2 = ('https://newsapi.org/v2/top-headlines?'
-#         'country=us&'
-#         f'apiKey={os.getenv("NEWS_API_KEY")}')
-#     response2 = requests.get(url2)
-#     print(response2.json())
-
-
-data_collection()
+    if response.status_code == 200:
+        return response.json()
+    else:
+        print(f"Failed to retrieve data from {url}. Status code: {response.status_code}")
+        return None
